86cd3ccf03c353ed07b0575c9575415cfd322146
Sid110307 <srsfool@gmail.com>
2024-01-19 12:31:43 +0530

    Updated polybar config

--- polybar/scripts/spotify.sh
+++ polybar/scripts/spotify.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+get_player_info() {
+  local status=$(playerctl -p "$1" status 2>/dev/null)
+  local metadata=$(playerctl -p "$1" metadata 2>/dev/null)
+
+  if [ -n "$metadata" ]; then
+    local artist=$(playerctl -p "$1" metadata artist)
+    local title=$(playerctl -p "$1" metadata title)
+
+    if [ "$status" = "Playing" ]; then
+      echo -e "\uf04c $artist - $title"
+    else
+      echo -e "\uf04b $artist - $title"
+    fi
+  else
+    echo ""
+  fi
+}
+
 if [ "$1" = "--toggle" ]; then
-	playerctl play-pause
+  playerctl play-pause
 elif [ "$1" = "--next" ]; then
-	playerctl next
+  playerctl next
 elif [ "$1" = "--prev" ]; then
-	playerctl previous
+  playerctl previous
 fi
 
-if [ -n "$(playerctl metadata)" ]; then
-	if [ "$(playerctl status)" = "Playing" ]; then
-		echo -e "\uf04c $(playerctl metadata artist) - $(playerctl metadata title)"
-	else
-		echo -e "\uf04b $(playerctl metadata artist) - $(playerctl metadata title)"
-	fi
+if [ -n "$(playerctl -p spotify metadata)" ]; then
+  get_player_info "spotify"
+elif [ -n "$(playerctl metadata)" ]; then
+  get_player_info ""
 else
-	echo ""
+  echo ""
 fi
